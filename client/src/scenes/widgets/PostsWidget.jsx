d18dda9d548fc2be8f31cb6fbbc26c00301042cf
RakibulHossainHimel <himel577303@gmail.com>
2024-12-01 03:47:17 +0600

    first commit

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -5,6 +5,7 @@ import PostWidget from "./PostWidget";
 
 const PostsWidget = ({ userId, isProfile = false }) => {
   const dispatch = useDispatch();
+  
   const posts = useSelector((state) => state.posts);
   const token = useSelector((state) => state.token);
 
